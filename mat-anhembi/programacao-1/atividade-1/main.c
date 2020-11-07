0ef3238609d3478246044cac7b461b92b7fabfb5
Pedro <schreiber.mmb@gmail.com>
2020-11-06 16:12:27 -0800

    add car filter, organize helper functions

--- mat-anhembi/programacao-1/atividade-1/main.c
+++ mat-anhembi/programacao-1/atividade-1/main.c
@@ -10,62 +10,40 @@ typedef struct Vehicle{
     char model[50];
 } Vehicle;
 
-//define global object
-Vehicle vehicles[10];
-
 //prototype helper functions
 int menuSelect();
 Vehicle vehicle(char model[50],char license[50], int year);
+Vehicle getVehicle();
+void init();
 void addVehicle(int idx);
-int filterVehicles(char model[50]);
+void filterVehicles();
 void listVehicles();
+
+//define global object
+Vehicle vehicles[10];
+
+void seed(){
+vehicles[0] = vehicle("palio","AHXH-1001",1999);
+vehicles[1] = vehicle("siena", "BHDS-1495",2000);
+vehicles[2] = vehicle("focus", "AEJE-6344",2018);
+vehicles[3] = vehicle("logan", "HDHD-3423",2018);
+vehicles[4] = vehicle("renegade", "OIEF-3423",2015);
+vehicles[5] = vehicle("corsa", "AEOI-2342",2015);
+vehicles[6] = vehicle("fusca", "AEGE-7464",2015);
+vehicles[7] = vehicle("uno", "AFEV-9874",2015);
+vehicles[8] = vehicle("celta", "AEKJ-3456",2015);
+vehicles[9] = vehicle("jeep", "AEDF-4523",2015);
+}
 
 //main program
 int main(){
-    bool running = true;
-
-    while(running == true){
-        switch (menuSelect()){
-            case 1:
-                printf("Add new!\n");
-                for (int i = 0; i < 10; i++){
-                    if (vehicles[i].year == 0){
-                        addVehicle(i);
-                        break;
-                    }
-                }
-                printf("%s - license plate %s registered successfully\n",vehicles[0].model, vehicles[0].license);
-                break;
-            case 2:
-                listVehicles();
-                break;
-            case 3:
-                printf("Bye!\n");
-                running = false;
-                break;
-            default:
-                printf("invalid option\n");
-        }
-    }
+    printf("Welcome to Car Register. In this program you can register your car and list all available cars.\n\n");
+    seed();
+    init();
     return 0;
 }
 
 //helper functions
-void addVehicle(int idx){
-    int year;
-    char model[50];
-    char license[50];
-
-    printf("Please inform the model:\n");
-    scanf("%s", &model);
-    printf("Please inform the license plate:\n");
-    scanf("%s", &license);
-    printf("Please inform the year:\n");
-    scanf("%d", &year);
-
-    vehicles[idx] = vehicle(model,license,year);
-}
-
 Vehicle vehicle(char model[50],char license[50], int year){
     Vehicle a;
     strcpy(a.license,license);
@@ -73,19 +51,53 @@ Vehicle vehicle(char model[50],char license[50], int year){
     a.year = year;
     return a;
 }
+Vehicle getVehicle(char query[50]){
+    Vehicle a;
+    for (int i = 0; i < 10; i++){
+        if (stricmp(vehicles[i].model,query) == 0) {
+            a = vehicles[i];
+        } 
+    }
+    return a;
+}
 
 int menuSelect(){
     int option;
-    printf("Select option\n1.Add new vehicle\n2.List vehicles\n3.Exit\n");
-    scanf("%d", &option);
+    printf("Select option\n1.Add new vehicle\n2.List vehicles\n3.Search vehicles\n4.Exit\n");
+    int scan = scanf("%d", &option);
     return option;
 }
 
-int filterVehicles(char model[50]){
-    //https://fresh2refresh.com/c-programming/c-passing-struct-to-function/
-    return 1;
-}
+void addVehicle(int idx){
+    int year;
+    char model[50];
+    char license[50];
 
+    printf("Please inform the model:\n");
+    int scanModel = scanf("%s", &model);
+    printf("Please inform the license plate:\n");
+    int scanLicense = scanf("%s", &license);
+    printf("Please inform the year:\n");
+    int scanYear = scanf("%d", &year);
+
+    if (scanYear >= 1 && scanModel >= 1 && scanLicense >=1) {
+        vehicles[idx] = vehicle(model,license,year);
+    } else {
+        printf("Error. Input correct values");
+        addVehicle(idx);
+    }
+}
+void filterVehicles(){
+    char query[50];
+    printf("What model are you looking for?\n");
+    scanf("%s",&query);
+    Vehicle searchResult = getVehicle(query);
+    if(searchResult.model[0] != '\0'){
+        printf("SEARCH RESULT: %s -- license %s\n\n", searchResult.model, searchResult.license);
+    } else {
+        printf("VEHICLE NOT FOUND\n\n");
+    }
+}
 void listVehicles(){
     printf("############################\n");
     for (int i = 0; i < 10; i++){
@@ -94,6 +106,35 @@ void listVehicles(){
         printf("%d\n",vehicles[i].year);
     }
     printf("############################\n");
+}
+void init(){
+        switch (menuSelect()){
+        case 1:
+            printf("Add new!\n");
+            for (int i = 0; i < 10; i++){
+                if (vehicles[i].year == 0){
+                    addVehicle(i);
+                    printf("REGISTERED SUCCESSFULLY: %s -- license plate %s\n\n",vehicles[i].model, vehicles[i].license);
+                    break;
+                }
+            }
+            init();
+            break;
+        case 2:
+            listVehicles();
+            init();
+            break;
+        case 3:
+            filterVehicles();
+            init();
+            break;
+        case 4:
+            printf("Bye!");
+            break;
+        default:
+            printf("invalid option\n\n");
+            init();
+    }
 }
 /*
 Em linguagens de programa��o estruturadas, tal como a linguagem C/C++ temos a possibilidade de organizar as informa��es a serem manipuladas sob dois aspectos:
