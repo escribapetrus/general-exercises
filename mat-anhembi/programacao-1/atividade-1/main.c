07a9d307b4a0b4567f63273a93e6ce9514d4d63b
Pedro <schreiber.mmb@gmail.com>
2020-11-08 11:42:50 -0800

    finish

--- mat-anhembi/programacao-1/atividade-1/main.c
+++ mat-anhembi/programacao-1/atividade-1/main.c
@@ -5,17 +5,17 @@
 //type definitions
 typedef enum {false, true} bool;
 typedef struct Vehicle{
-    int year;
-    char license[50];
+    char brand[50];
     char model[50];
+    char license[50];
+    int year;
 } Vehicle;
 
 //prototype helper functions
 int menuSelect();
-
 int partition(Vehicle arr[], int low, int high);
 
-Vehicle vehicle(char model[50],char license[50], int year);
+Vehicle vehicle(char brand[50],char model[50],char license[50],int year);
 Vehicle getVehicle(Vehicle arr[], char query[50]);
 
 void swap(Vehicle* a, Vehicle* b);
@@ -26,29 +26,26 @@ void addVehicle(Vehicle arr[]);
 void filterVehicles();
 void listVehicles(Vehicle arr[]);
 
-//define global object
-// Vehicle vehicles[10];
-
 //main program
 int main(){
     bool running = true;
     Vehicle vehicles[10] = {
-        vehicle("", "", 0),
-        vehicle("", "", 0),
-        vehicle("", "", 0),
-        vehicle("", "", 0),
-        vehicle("", "", 0),
-        vehicle("", "", 0),
-        vehicle("", "", 0),
-        vehicle("", "", 0),
-        vehicle("", "", 0),
-        vehicle("", "", 0)
+        vehicle("","","",0),
+        vehicle("","","",0),
+        vehicle("","","",0),
+        vehicle("","","",0),
+        vehicle("","","",0),
+        vehicle("","","",0),
+        vehicle("","","",0),
+        vehicle("","","",0),
+        vehicle("","","",0),
+        vehicle("","","",0)
     };
 
     printf("Welcome to Car Register. In this program you can register your car and list all available cars.\n\n");
     seed(vehicles);
-    quickSort(vehicles, 0, sizeof(vehicles)/sizeof(vehicles[0])-1);
     while(running == true){
+        quickSort(vehicles, 0, sizeof(vehicles)/sizeof(vehicles[0])-1);
         switch (menuSelect()){
             case 1:
                 addVehicle(vehicles);
@@ -67,7 +64,6 @@ int main(){
                 printf("invalid option\n\n");
         }
     }
-    
     return 0;
 }
 int menuSelect(){
@@ -90,10 +86,11 @@ int partition(Vehicle arr[], int low, int high){
     return (i + 1);
 }
 
-Vehicle vehicle(char model[50],char license[50], int year){
+Vehicle vehicle(char brand[50],char model[50],char license[50], int year){
     Vehicle a;
-    strcpy(a.license,license);
+    strcpy(a.brand,brand);
     strcpy(a.model, model);
+    strcpy(a.license,license);
     a.year = year;
     return a;
 }
@@ -113,9 +110,12 @@ void addVehicle(Vehicle arr[]){
         if (arr[i].year == 0){
             full = false;
             int year;
+            char brand[50];
             char model[50];
             char license[50];
             printf("Add new!\n");
+            printf("Please inform the model:\n");
+            int scanBrand = scanf("%s", &brand);
             printf("Please inform the model:\n");
             int scanModel = scanf("%s", &model);
             printf("Please inform the license plate:\n");
@@ -124,7 +124,7 @@ void addVehicle(Vehicle arr[]){
             int scanYear = scanf("%d", &year);
 
             if (scanYear >= 1 && scanModel >= 1 && scanLicense >=1) {
-                arr[i] = vehicle(model,license,year);
+                arr[i] = vehicle(brand,model,license,year);
                 printf("REGISTERED SUCCESSFULLY: %s -- license plate %s\n\n",arr[i].model, arr[i].license);
             } else {
                 printf("Error. Input correct values");
@@ -149,23 +149,19 @@ void filterVehicles(Vehicle arr[]){
 void listVehicles(Vehicle arr[]){
     printf("############################\n");
     for (int i = 0; i < 10; i++){
-        printf("%s -- ",arr[i].model);
-        printf("%s -- ",arr[i].license);
-        printf("%d\n",arr[i].year);
+        if (arr[i].year != 0) {
+            printf("%d. %s %s, license plate: %s, year: %d\n",i+1,arr[i].brand,arr[i].model,arr[i].license, arr[i].year);
+        }
     }
     printf("############################\n");
 }
 void seed(Vehicle arr[]){
-arr[0] = vehicle("palio","AHXH-1001",1999);
-arr[1] = vehicle("siena","BHDS-1495",2000);
-arr[2] = vehicle("focus","AEJE-6344",2018);
-arr[3] = vehicle("logan","HDHD-3423",2016);
-arr[4] = vehicle("renegade","OIEF-3423",2020);
-arr[5] = vehicle("fusca","AEGE-7464",1967);
-arr[6] = vehicle("uno","AFEV-9874",1985);
-arr[7] = vehicle("celta","AEKJ-3456",2005);
-// vehicles[5] = vehicle("corsa","AEOI-2342",2015);
-// vehicles[9] = vehicle("jeep","AEDF-4523",2015);
+arr[0] = vehicle("fiat","palio","AHXH-1001",1999);
+arr[2] = vehicle("ford","focus","AEJE-6344",2018);
+arr[3] = vehicle("renault","logan","HDHD-3423",2016);
+arr[4] = vehicle("jeep","renegade","OIEF-3423",2020);
+arr[5] = vehicle("wolkswagen","fusca","AEGE-7464",1967);
+arr[6] = vehicle("fiat","uno","AFEV-9874",1985);
 }
 void swap(Vehicle* a, Vehicle* b){
     // printf("\nSWAPPED!\n");
