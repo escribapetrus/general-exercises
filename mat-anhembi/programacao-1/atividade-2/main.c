7f2f48c14ba7ccdd15d147a4c0d011e9863744c1
Pedro <schreiber.mmb@gmail.com>
2020-11-24 19:51:35 -0800

    remove client done and improved

--- mat-anhembi/programacao-1/atividade-2/main.c
+++ mat-anhembi/programacao-1/atividade-2/main.c
@@ -4,7 +4,6 @@
 
 typedef enum {false, true} bool;
 typedef struct Client{
-    int id;
     char name[30];
     int year;
     int amount;
@@ -13,8 +12,7 @@ typedef struct Client{
 int arr_size = 10;
 int clients_registered = 0;
 
-Client client(int id, char name[30], int year, int amount);
-Client getClient(int id, Client *list);
+Client client(char name[30], int year, int amount);
 int menu();
 
 void addClient(Client *list);
@@ -34,28 +32,29 @@ int main(int argc, char const *argv[]){
         }
         switch (menu()){
         case 1:
-            printf("Option 1\n");
             addClient(clients);
             break;
         case 2:
-            printf("Option 2\n\n");
             removeClient(clients);
             break;
         case 3:
             printf("Option 3\n\n");
             break;        
         case 4:
-            printf("Option 4\n\n");
             seed(clients);
             break;
         case 5:
-            printf("id   name    year     amount\n");
-            for (size_t i = 0; i < clients_registered; i++) {
-                printf("%d. %s -- %d -- %d\n", clients[i].id, clients[i].name,clients[i].amount,clients[i].year);
+            if (clients_registered == 0) {
+                printf("No clients registered yet.\n\n");
+            } else {
+                printf("id   name    year     amount\n");
+                for (size_t i = 0; i < clients_registered; i++) {
+                    printf("%d. %s -- %d -- %d\n",i+1,clients[i].name,clients[i].amount,clients[i].year);
+                }
             }
             break;
         case 6:
-            printf("Option 6\n\n");
+            printf("Bye!");
             running = false;
             break;
         default:
@@ -66,20 +65,12 @@ int main(int argc, char const *argv[]){
     return 0;
 }
 
-Client client(int id, char name[30], int year, int amount){
+Client client(char name[30], int year, int amount){
     Client c;
-    c.id = id;
     strcpy(c.name,name);
     c.year = year;
     c.amount = amount;
     return c;
-}
-Client getClient(int id, Client *list){
-    // Client c;
-    // for (int i = id - 1; i < clients_registered; i++){
-    //     list[i] = list[id]; 
-    // }
-    // clients_registered = clients_registered - 1; 
 }
 
 int menu(){
@@ -103,36 +94,33 @@ void addClient(Client *list){
 
     if (scanN >= 1 && scanY >= 1 && scanA >= 1) {
         printf("Client added successfully\n\n");
-        list[clients_registered] = client(clients_registered + 1,name,year,amount);
+        list[clients_registered] = client(name,year,amount);
         clients_registered = clients_registered + 1;
     } else {
         printf("Incorrect values, try again.\n\n");
         addClient(list);
     }
 }
-
 void removeClient(Client *list){
     int target;
     printf("Inform id of the client to remove.\n");
     scanf("%d", &target);
     for (int i = target; i < clients_registered; i++){
-        strcpy(list[i - 1].name, list[i].name); 
-        list[i - 1].year = list[i].year; 
-        list[i - 1].amount = list[i].amount; 
+        list[i] = list[i+1]; 
     }
     clients_registered = clients_registered - 1; 
 }
-
 void seed(Client *list){
-    list[0] = client(1,"clara", 19967, 30000);
-    list[1] = client(2,"amanda", 1970, 20000);
-    list[2] = client(3,"julia", 1993, 65000);
-    list[3] = client(4,"jenny", 1991, 453000);
-    list[4] = client(5,"rafaela", 1985, 323000);
-    list[5] = client(6,"josefina", 1997, 123000);
-    list[6] = client(7,"margarida", 1950, 24000);
-    list[7] = client(8,"isabela", 2000, 86000);
-    list[8] = client(9,"rosario", 1930, 90000);
-    // list[9] = client(10,"hayley", 1998, 902314000);
+    list[0] = client("clara", 19967, 30000);
+    list[1] = client("amanda", 1970, 20000);
+    list[2] = client("julia", 1993, 65000);
+    list[3] = client("jenny", 1991, 453000);
+    list[4] = client("rafaela", 1985, 323000);
+    list[5] = client("josefina", 1997, 123000);
+    list[6] = client("margarida", 1950, 24000);
+    list[7] = client("isabela", 2000, 86000);
+    list[8] = client("rosario", 1930, 90000);
+    // list[9] = client("hayley", 1998, 902314000);
     clients_registered = 9;
+    printf("Added 9 records!\n\n");
 }
